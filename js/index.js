af315f908be2e5af4e287327101168b3315b03c6
Magnar <125993203+maglus02@users.noreply.github.com>
2023-03-31 15:28:52 +0200

    Inital HTML and CSS CA commit

---
+++ js/index.js
@@ -0,0 +1,7 @@
+function toggleMobileNav(menu) {
+    menu.classList.toggle('open');
+}
+
+function deliveryEmailInfo() {
+    alert('We need your email so that we can send you the digital code to your game.');
+}
