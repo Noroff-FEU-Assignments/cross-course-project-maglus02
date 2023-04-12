1d628fb527f837da6fac7448acd8038e9f68377a
Magnar <125993203+maglus02@users.noreply.github.com>
2023-04-12 14:30:42 +0200

    Validator fixes

--- js/index.js
+++ js/index.js
@@ -1,7 +1,3 @@
-function toggleMobileNav(menu) {
+function mobileNav(menu) {
     menu.classList.toggle('open');
-}
-
-function deliveryEmailInfo() {
-    alert('We need your email so that we can send you the digital code to your game.');
 }
